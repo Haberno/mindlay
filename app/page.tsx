59f767c1d4437eaf63642b4bdb3c6188d32c0aeb
Sam120204 <75916625+Sam120204@users.noreply.github.com>
2024-05-18 14:37:54 -0400

    Update the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,25 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import {useUser, useRedirectFunctions, useLogoutFunction} from "@propelauth/nextjs/client";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+const WelcomeMessage = () => {
+    const {loading, user} = useUser()
+    const {redirectToSignupPage, redirectToLoginPage, redirectToAccountPage} = useRedirectFunctions()
+    const logoutFn = useLogoutFunction()
+
+    if (loading) return <div>Loading...</div>
+
+    if (user) {
+        return <div>
+            <p>You are logged in as {user.email}</p>
+            <button onClick={() => redirectToAccountPage()}>Account</button>
+            <button onClick={logoutFn}>Logout</button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    } else {
+        return <div>
+            <p>You are not logged in</p>
+            <button onClick={() => redirectToLoginPage()}>Login</button>
+            <button onClick={() => redirectToSignupPage()}>Signup</button>
+        </div>
+    }
 }
+export default WelcomeMessage
